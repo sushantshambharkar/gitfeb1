dfd86c58a3476ae2de3d44b9ade2d9f0c012da35
sushantshambharkar <40300856+sushantshambharkar@users.noreply.github.com>
2020-02-01 17:12:13 -0600

    Initial copy

---
+++ feb1/src/main/java/com/itexps/marketing/Car.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.itexps.marketing;
+
+/**
+ *
+ * @author Sushant
+ */
+public class Car {
+    String vin ;
+    String color;
+
+    public String getVin() {
+        return vin;
+    }
+
+    public void setVin(String vin) {
+        this.vin = vin;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+    
+    
+    
+}
